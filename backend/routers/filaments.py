e64fa356204d0c3374c85b67cff2a4f8df7fab9b
Leone Raulino Silveira <leone.zordenoni@gmail.com>
2025-06-08 21:06:20 -0400

    feat: edit and cancel feature

--- backend/routers/filaments.py
+++ backend/routers/filaments.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from services.scraper import get_mercadolivre_filaments, get_amazon_filaments
+from services.scraper import get_mercadolivre_filaments
 from schemas.filament import FilamentSchema
 
 router = APIRouter()
@@ -8,8 +8,3 @@ router = APIRouter()
 @router.get("/mercadolivre", response_model=list[FilamentSchema])
 async def get_filaments_mercadolivre():
     return get_mercadolivre_filaments()
-
-
-@router.get("/amazon", response_model=list[FilamentSchema])
-async def get_filaments_amazon():
-    return get_amazon_filaments()
--- frontend/Filamento.py
+++ frontend/Filamento.py
@@ -1,40 +1,26 @@
 import os
 from dotenv import load_dotenv   # type: ignore
 from flask import Flask, render_template, request, redirect, session, flash, url_for
-
-load_dotenv()
-
-
-class Filament:
-    def __init__(self, color, supplier, description, amount):
-        self.color = color
-        self.supplier = supplier
-        self.description = description
-        self.amount = amount
+from model import Filament, User
 
 
-Filament1 = Filament('Vermelho', 'Volt', 'Vermelho', '1000')
-Filament2 = Filament('Azul piscina', 'National', 'Azul tiffany', '500')
-Filament3 = Filament('Branco', 'National', 'Branco gelo', '300')
-list = [Filament1, Filament2, Filament3]
-
-
-class User:
-    def __init__(self, name, nickname, password):
-        self.name = name
-        self.nickname = nickname
-        self.password = password
-
+filament_list = [
+    Filament(1, 'Vermelho', 'Volt', 'Vermelho', '1000', True),
+    Filament(2, 'Azul piscina', 'National', 'Azul tiffany', '500', True),
+    Filament(3, 'Branco', 'National', 'Branco gelo', '300', True)
+]
 
 list_users = [
     User('Luiza', 'Lu', '1234'),
     User('Leone', 'Leo', '1235'),
-    User('Laura', 'Laurita', '1236')]
+    User('Laura', 'Laurita', '1236')
+]
 
 users = {user.nickname: user for user in list_users}
 
+load_dotenv()
 app = Flask(__name__)
-app.secret_key = os.getenv("API_KEY")
+app.secret_key = os.getenv("SECRET_KEY")
 
 
 @app.route('/')
@@ -42,25 +28,62 @@ def index():
     return render_template(
         'lista.html',
         title='Filaments 3D Silveira',
-        Filaments=list)
+        Filaments=filament_list)
 
 
-@app.route('/new')
+@app.route('/create', methods=['POST',])
+def create():
+    new_id = max(f.id for f in filament_list) + 1 if filament_list else 1
+    color = request.form['color']
+    supplier = request.form['supplier']
+    description = request.form['description']
+    amount = request.form['amount']
+    Filament4 = Filament(new_id, color, supplier, description, amount, True)
+    filament_list.append(Filament4)
+    return redirect(url_for('index'))
+
+
+@app.route('/new', methods=['GET', 'POST'])
 def new():
     if 'user_logged_in' not in session or session['user_logged_in'] is None:
         return redirect(url_for('login', following=url_for('new')))
     return render_template('cadastro.html', title='Filament registration')
 
 
-@app.route('/create', methods=['POST',])
-def create():
-    color = request.form['color']
-    supplier = request.form['supplier']
-    description = request.form['description']
-    amount = request.form['amount']
-    Filament4 = Filament(color, supplier, description, amount)
-    list.append(Filament4)
-    return redirect(url_for('index'))
+@app.route('/delete/<int:id>', methods=['GET', 'POST'])
+def delete(id=None):
+    if 'user_logged_in' not in session:
+        return redirect(url_for('login', following=request.url))
+    for filament in filament_list:
+        if (filament.id == id):
+            filament.activate = False
+            flash("Filament deleted")
+            return redirect(url_for('index'))
+
+
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id=None):
+    if 'user_logged_in' not in session:
+        return redirect(url_for('login', following=request.url))
+
+    filament = next((f for f in filament_list if f.id == id), None)
+    if not filament:
+        flash("Filament not found")
+        return redirect(url_for('index'))
+
+    if request.method == 'POST':
+        color = request.form['color']
+        supplier = request.form['supplier']
+        description = request.form['description']
+        amount = request.form['amount']
+        if filament:
+            filament.color = color
+            filament.supplier = supplier
+            filament.description = description
+            filament.amount = amount
+            flash("Filament updated")
+        return redirect(url_for('index'))
+    return render_template('cadastro.html', filament=filament)
 
 
 @app.route('/login')
@@ -69,18 +92,19 @@ def login():
     return render_template('login.html', following=following)
 
 
-@app.route('/authenticate', methods=['POST', ])
+@app.route('/authenticate', methods=['POST'])
 def authenticate():
     user_input = request.form['user']
     password_input = request.form['password']
 
+    following = request.form.get('following')
     if user_input in users:
         user = users[user_input]
         if password_input == user.password:
             session['user_logged_in'] = user.nickname
-            flash(user.nickname + ' logged in')
-            return redirect(url_for('new'))
-    flash('User not logged in')
+            flash(f'{user.nickname} logged in')
+            return redirect(following or url_for('index'))
+    flash('Usuário ou senha inválidos')
     return redirect(url_for('login'))
 
 
@@ -92,3 +116,6 @@ def logout():
 
 
 app.run(debug=True, port=5002)
+
+if __name__ == '__main__':
+    app.run(debug=True)
