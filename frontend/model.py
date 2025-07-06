68f1ff9fdfca91dd3e4717e4be3d6605bb244d73
Leone Raulino Silveira <leone.zordenoni@gmail.com>
2025-07-06 16:02:49 -0400

    feat: integration part

--- frontend/model.py
+++ frontend/model.py
@@ -6,10 +6,20 @@ class User:
 
 
 class Filament:
-    def __init__(self, id, color, supplier, description, amount, activate):
+    def __init__(self, id, color, brand, description, amount, activate):
         self.id = id
         self.color = color
-        self.supplier = supplier
+        self.brand = brand
         self.description = description
         self.amount = amount
         self.activate = activate
+
+# from typing import TypedDict
+
+# class Filament(TypedDict):
+#     id: int
+#     filament_name: str
+#     color: str
+#     brand: str
+#     quantity: float
+#     activate: bool
